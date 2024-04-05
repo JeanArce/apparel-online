d3ee93ac022b4c7715c8fbb71f80f03da85c957f
JeanArce <113121403+JeanArce@users.noreply.github.com>
2024-04-05 17:00:47 +0200

    search product

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -5,33 +5,60 @@ import './home.styles.scss';
 
 const Home = () => {
 
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [productsList, setProductsList] = useState([]);
 
-    useEffect(() => {
-        const getProducts = async() => {
-            const productsJson = await fetchProducts();
-
-            console.log(productsJson);
-            setProducts(productsJson.data);
-        };
-        getProducts();
-    }, []);
 
   
-    return (
-      <Fragment>
-        <h2 className="category-title">Products</h2>
+  const handleSearch = (e) => {
 
-        {
+    const searchTerm = e.target.value.toLowerCase();
+    const filteredProducts = products.filter((product) =>
+      product.title.toLowerCase().includes(searchTerm)
+    );
+
+    setProductsList(filteredProducts);
+
+  };
+
+  useEffect(() => {
+    const getProducts = async() => {
+      const productsJson = await fetchProducts();
+
+        
+      setProducts(productsJson.data);
+      setProductsList(productsJson.data);
+
+    };
+    getProducts();
+  }, []);
+
+  
+  return (
+    <Fragment>
+      <h2 className="category-title">Products</h2>
+
+      <input
+        type="text"
+        className="searchInput"
+        name="searchInput"
+        placeholder="Search Product"
+        onChange={handleSearch}
+      />
+
+      {
         <div className="category-container">
-          {products &&
-            products.map((product) => (
+          {productsList &&
+            productsList.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
         </div>
-        }
-      </Fragment>
-    );
+      }
+    </Fragment>
+  );
+
+
+
 };
 
 
